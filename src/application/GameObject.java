0af2e7c7c5b7c2101302d1ebfdb9649a7a6d41e3
Vladimir <ajantis@centrum.cz>
2019-03-26 11:36:12 +0000

    /*polished the code and got it ready to upload to github

--- src/application/GameObject.java
+++ src/application/GameObject.java
@@ -5,19 +5,18 @@ import javafx.geometry.Point2D;
 import javafx.scene.Node;
 
 public class GameObject {
-	private Node view;
-	private Point2D velocity = new Point2D(0,0);
-	private int rotationSpeed = 5;
-	
-	private boolean alive = true;
+	private Node view; // Used for collision detection and other animation features
+	private Point2D velocity = new Point2D(0,0); // For setting the direction the object will move
+	private int rotationSpeed = 5; // rotation speed attribute for object just to have easy way to control it
+	private boolean alive = true; // for object to know if is dead or alive and remove if dead
 	
 	public GameObject(Node view) {
 		this.view = view;
 	}
+	//for updating the position, movement of the object
 	public void update() {
 		view.setTranslateX(view.getTranslateX() + velocity.getX());
 		view.setTranslateY(view.getTranslateY() + velocity.getY());
-
 	}
 
 	public Node getView() {
@@ -66,7 +65,7 @@ public class GameObject {
 	public void moveDown() {
 		//this.velocity = new Point2D(velocity.getX() ,velocity.getY()+1);
 	}
-	
+	// to be used later on in development for rotating object
 	public void rotateRight() {
 		view.setRotate(view.getRotate() + rotationSpeed);
 		setVelocity(new Point2D(Math.cos(Math.toRadians(getRotate())), Math.sin(Math.toRadians(getRotate()))));
@@ -76,11 +75,10 @@ public class GameObject {
 		view.setRotate(view.getRotate() - rotationSpeed);
 		setVelocity(new Point2D(Math.cos(Math.toRadians(getRotate())), Math.sin(Math.toRadians(getRotate()))));
 	}
-	
+	// check for collision, comparing the 2 objects boundries
 	public boolean isColliding (GameObject other) {
 		return getView().getBoundsInParent().intersects(other.getView().getBoundsInParent());
 	}
-	
 	
 }//End Game object
 
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,3 +1,8 @@
+/*
+ * Simple game to learn principles. Graphics are basic as they are not important at this stage. Its survival
+ * game where you try to kill enemies before they touch you. The more you kill the harder the game gets.
+ * Simple controls of WASD for moving and left mouse button for shooting.
+ */
 package application;
 	
 import java.util.List;
@@ -7,51 +12,41 @@ import java.util.ArrayList;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.stage.Stage;
-import sun.security.mscapi.KeyStore.ROOT;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
-
 public class Main extends Application {
 	static private int height = 600;
 	static private int width = 600;
-	static private int enemiesKilled = 0;
+	static private int enemiesKilled = 0; // counter for enemies killed
 	private Pane root;
-	private List<GameObject> enemies = new ArrayList<>();
-	private List<GameObject> bullets = new ArrayList<>(); 
+	private GameObject player; // player object
+	private List<GameObject> enemies = new ArrayList<>(); //holds all the enemies
+	private List<GameObject> bullets = new ArrayList<>(); //holds all the bullets
+	// score and timer shower, had to be done outside of body
 	Text scoreBoard = new Text(0,0, ("Enemies Killed " + enemiesKilled));
-	
 	final long starMiliTime = System.currentTimeMillis();
 	long currentTime = System.currentTimeMillis();
 	Text timer = new Text(100,100,"Survived for " +(currentTime - starMiliTime) + "seconds" );
-	
-	private GameObject player;
-	
+	// function for content creation thats called in start, just to make the code easier to read
 	private Parent createContent() {
 		root = new Pane();
 		root.setPrefSize(width,height);
+		// ensuring they are always 0 and 1 position
 		root.getChildren().add(scoreBoard);
 		root.getChildren().add(timer);
+		//player adding and position
 		player = new Player();
-		//player.setVelocity(new Point2D( 0 ,0));
 		addGameObject(player, width/2, height/2);
 		
 		AnimationTimer timer = new AnimationTimer() {
@@ -61,8 +56,6 @@ public class Main extends Application {
 			}
 		};
 		timer.start();
-		
-		
 		return root;
 	}
 	
@@ -81,7 +74,9 @@ public class Main extends Application {
 		enemies.add(enemy);
 		addGameObject(enemy, x, y);
 	}
-
+	/*player with overriden controls from super class,
+	TODO replace circle with graphic and then add method under update that will rotate the player towards
+	TODO the mouse*/
 	private static class Player extends GameObject{
 		public Player() {
 			super(new Circle(4,5,15, Color.MIDNIGHTBLUE) );
@@ -116,15 +111,15 @@ public class Main extends Application {
 		}
 	}
 	
+	
+	// main function for graphics
 	private void onUpdate() {
-		
-		// scoreboard
+		// score board, just replacing the old one with new 
 		Text scoreBoard = new Text(0,20, ("Enemies Killed " + enemiesKilled));
 		scoreBoard.setFill(Color.BLACK);
 		scoreBoard.setFont(Font.font(20));
 		root.getChildren().set(0, scoreBoard);
-		
-		
+		// stop the app when the player is dead, and also while running it shows time how long you are alive		
 		if (player.isDead()) {
 			Text text = new Text(width/2-100,height/2,"Game Over");
 			text.setFill(Color.RED);
@@ -139,6 +134,7 @@ public class Main extends Application {
 			timer.setFont(Font.font(20));
 			root.getChildren().set(1, timer);
 		}
+		
 		player.update();
 		
 		//boundries for the player
@@ -151,14 +147,14 @@ public class Main extends Application {
 		else if (player.getView().getTranslateY() >= (root.getPrefHeight() -20 ) ) 
 		{player.getView().setTranslateY(root.getPrefHeight()-20);}
 	
-		
+		//updating positions of bullets
 		for(GameObject bullet: bullets) {
 			bullet.update();
 		}
-		
+		/*//updating positions of enemies, first inner loop checks for positions of enemy and bullet
+		 * and they match they are both set as dead and the counter for kills go up by 1
+		*/
 		for (GameObject enemy: enemies) {
-			
-			
 			for (GameObject bullet: bullets) {
 				if (bullet.isColliding(enemy)) {
 					bullet.setDead(true);
@@ -167,23 +163,20 @@ public class Main extends Application {
 					enemiesKilled++;
 				}
 			}
-			
-			
-			
-			// guiding system for enemies
+			// guiding system for enemies, its pretty much is getting to players position and then the speed
+			// of enemies is adjusted
 			if (player.isColliding(enemy)){player.setDead(true);}
 			enemy.setVelocity(new Point2D(player.getView().getTranslateX()-enemy.getView().getTranslateX()
 					,player.getView().getTranslateY()- enemy.getView().getTranslateY())
 					.normalize().multiply(0.5));
 			enemy.update();
-			
 		}
-		
+		// removing the enemies and the bullets which are dead
 		bullets.removeIf(GameObject::isDead);
 		enemies.removeIf(GameObject::isDead);
-		
-		
-		// random generator for enemies
+		/* random generator for enemies, it goes beyond the minimal spawning rate and changes 
+		 * to faster spawn rate according to kill count. Its divided to 4 sides for top,bottom,left and right
+		*/
 		if (Math.random() < enemiesKilled * 0.001 || Math.random() < 0.01) {
 			if (Math.random() < 0.25) {
 				addEnemy(new Enemy(), 0, Math.random() * root.getPrefHeight());
@@ -195,14 +188,12 @@ public class Main extends Application {
 				addEnemy(new Enemy(), Math.random() * root.getPrefWidth(), height-16);
 			}
 		}
-		
-		
 	}
 	
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setScene(new Scene(createContent()));
-		//controls
+		//controls for movement
 		stage.getScene().setOnKeyPressed( e ->{
 			if (e.getCode() == KeyCode.A) {
 				player.moveLeft();
@@ -214,25 +205,21 @@ public class Main extends Application {
 				player.moveDown();
 			} 
 		});
+		//controls for shooting, 
 		stage.getScene().setOnMouseClicked(e -> {
 			if (e.getButton() == MouseButton.PRIMARY) {
 				Bullet bullet = new Bullet();
 				// get position of the mouse, then - player position allow to shoot where I aim with mouse			
-				
 				bullet.setVelocity(new Point2D((e.getX()-player.getView().getTranslateX())
 						,(e.getY()-player.getView().getTranslateY()))
 						.normalize().multiply(3));
 				addBullet(bullet, player.getView().getTranslateX(), player.getView().getTranslateY());
-				
 			}
 		});
-	
 		stage.show();
-		
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
-
 }
